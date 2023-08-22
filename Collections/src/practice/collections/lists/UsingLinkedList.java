9e96ca947b8035072764af0b847581b51fd23452
Yovani <yovanitoribio@gmail.com>
2023-08-21 23:52:11 -0600

    updated the collections setction

--- Collections/src/practice/collections/lists/UsingLinkedList.java
+++ Collections/src/practice/collections/lists/UsingLinkedList.java
@@ -1,5 +1,11 @@
 package practice.collections.lists;
 
+import java.util.LinkedList;
+
 public class UsingLinkedList {
 
+
+    public static void main(String[] args) {
+        LinkedList<String> myFirstLinkedList = new LinkedList<>();
+    }
 }
--- Collections/src/practice/collections/sets/UsingHashSets.java
+++ Collections/src/practice/collections/sets/UsingHashSets.java
@@ -1,4 +1,11 @@
 package practice.collections.sets;
 
+import java.util.Stack;
+import java.util.Vector;
+
 public class UsingHashSets {
+
+    Stack<String> myFirstStack = new Stack<String>();
+    Vector<String> myFirstVector = new Vector<>();
+
 }
